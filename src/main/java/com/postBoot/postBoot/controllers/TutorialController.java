ded150a5bd5c6ab7de4aec538baa36b6655ec640
tamani phiri <tamanigabriel0@gmail.com>
2023-10-02 10:26:26 +0200

    changes

---
+++ src/main/java/com/postBoot/postBoot/controllers/TutorialController.java
@@ -0,0 +1,7 @@
+package com.postBoot.postBoot.controllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin(origins = "http://localhost:8081")
+public class TutorialController  {
+}
