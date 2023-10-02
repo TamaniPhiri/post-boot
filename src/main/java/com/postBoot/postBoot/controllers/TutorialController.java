7347e7581b7425f44ac3730e3b9b953596dec64b
tamani phiri <tamanigabriel0@gmail.com>
2023-10-02 19:45:18 +0200

    changes

--- src/main/java/com/postBoot/postBoot/controllers/TutorialController.java
+++ src/main/java/com/postBoot/postBoot/controllers/TutorialController.java
@@ -50,6 +50,8 @@ public class TutorialController  {
         try {
             Tutorial _tutorial=tutorialRepository.save(new Tutorial(tutorial.getTitle(),tutorial.getDescription(),false));
             return new ResponseEntity<>(_tutorial,HttpStatus.CREATED);
+        }catch (Exception e){
+            return new ResponseEntity<>(null,HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
