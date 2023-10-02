45416e8e4dd63978ea00b350ec2c763ab4bf12ac
tamani phiri <tamanigabriel0@gmail.com>
2023-10-02 10:27:25 +0200

    changes

--- src/main/java/com/postBoot/postBoot/controllers/TutorialController.java
+++ src/main/java/com/postBoot/postBoot/controllers/TutorialController.java
@@ -1,5 +1,7 @@
 package com.postBoot.postBoot.controllers;
 
+import com.postBoot.postBoot.repository.TutorialRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,4 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class TutorialController  {
+    @Autowired
+    TutorialRepository tutorialRepository;
 }
