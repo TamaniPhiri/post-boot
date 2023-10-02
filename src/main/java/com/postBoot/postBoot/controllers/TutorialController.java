9a4d2cd4ba4a2b32697f54026e6dd16eb843cd2b
tamani phiri <tamanigabriel0@gmail.com>
2023-10-02 19:32:53 +0200

    changes

--- src/main/java/com/postBoot/postBoot/controllers/TutorialController.java
+++ src/main/java/com/postBoot/postBoot/controllers/TutorialController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @CrossOrigin(origins = "http://localhost:8081")
 @RestController
@@ -31,6 +32,16 @@ public class TutorialController  {
             return new ResponseEntity<>(tutorials,HttpStatus.OK);
         }catch (Exception e){
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/tutorial/{id}")
+    public ResponseEntity<Tutorial> getTutorialById(@PathVariable("id") long id){
+        Optional<Tutorial> tutorialData= tutorialRepository.findById(id);
+        if (tutorialData.isPresent()) {
+            return new ResponseEntity<>(tutorialData.get(),HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
 }
