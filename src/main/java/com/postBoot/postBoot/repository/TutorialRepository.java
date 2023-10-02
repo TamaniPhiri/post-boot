59fa20371cf4113a3c6e1baacb13e02f6ad1fa4e
tamani phiri <tamanigabriel0@gmail.com>
2023-10-02 10:22:51 +0200

    changes

--- src/main/java/com/postBoot/postBoot/repository/TutorialRepository.java
+++ src/main/java/com/postBoot/postBoot/repository/TutorialRepository.java
@@ -1,4 +1,10 @@
 package com.postBoot.postBoot.repository;
 
-public class TutorialRepository {
+import com.postBoot.postBoot.model.Tutorial;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TutorialRepository extends JpaRepository<Tutorial,Long> {
+    List<Tutorial> findByPublished(boolean published);
 }
