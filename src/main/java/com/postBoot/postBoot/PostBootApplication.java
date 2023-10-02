63c97affc1ad3f514e101f3d1c89d8a118ac26e3
tamani phiri <tamanigabriel0@gmail.com>
2023-10-02 09:42:49 +0200

    changes

---
+++ src/main/java/com/postBoot/postBoot/PostBootApplication.java
@@ -0,0 +1,13 @@
+package com.postBoot.postBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/postBoot/postBoot/PostBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.postBoot.postBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
