ee655ddccb4d0568949c7f8c74aabb75678e5884
tamani phiri <tamanigabriel0@gmail.com>
2023-10-02 10:38:30 +0200

    changes

--- src/main/java/com/postBoot/postBoot/controllers/TutorialController.java
+++ src/main/java/com/postBoot/postBoot/controllers/TutorialController.java
@@ -4,7 +4,6 @@ import com.postBoot.postBoot.model.Tutorial;
 import com.postBoot.postBoot.repository.TutorialRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
