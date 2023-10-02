648936c415f4ff58ba9d8263c4367f56159be070
tamani phiri <tamanigabriel0@gmail.com>
2023-10-02 10:26:52 +0200

    changes

--- src/main/java/com/postBoot/postBoot/controllers/TutorialController.java
+++ src/main/java/com/postBoot/postBoot/controllers/TutorialController.java
@@ -1,7 +1,11 @@
 package com.postBoot.postBoot.controllers;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin(origins = "http://localhost:8081")
+@RestController
+@RequestMapping("/api")
 public class TutorialController  {
 }
