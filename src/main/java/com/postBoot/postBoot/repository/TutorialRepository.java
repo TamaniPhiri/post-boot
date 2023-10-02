e451b525d7aab56a729dd3796906c181af6a17b9
tamani phiri <tamanigabriel0@gmail.com>
2023-10-02 10:23:55 +0200

    changes

--- src/main/java/com/postBoot/postBoot/repository/TutorialRepository.java
+++ src/main/java/com/postBoot/postBoot/repository/TutorialRepository.java
@@ -7,4 +7,6 @@ import java.util.List;
 
 public interface TutorialRepository extends JpaRepository<Tutorial,Long> {
     List<Tutorial> findByPublished(boolean published);
+
+    List<Tutorial> findByTitleContaining(String title);
 }
